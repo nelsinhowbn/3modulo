ba49cd17459c0d64466be1bc9f9b43f797ce4afa
Aluno <nelsonhadi95@gmail.com>
2018-08-14 22:23:38 -0300

    MAVEN

---
+++ PCI2/14.08/projetomvc/src/main/java/view/AlunosExec.java
@@ -0,0 +1,33 @@
+
+package view;
+
+import java.sql.Connection;
+
+import controller.AlunosJdbcDAO;
+import controller.JdbUtil;
+import model.Alunos;
+
+public class AlunosExec {
+public static void main(String args[]) {
+	Alunos alunos = new Alunos();
+	try {
+		alunos.setNome("Vinicius Alves");
+		alunos.setEndereco("Av Lopes, 2700");
+		alunos.setBairro("Penha");
+		alunos.setCep(10514557);
+		
+		
+		Connection connection = JdbUtil.getConnection();
+		AlunosJdbcDAO alunosJdbcDAO = new AlunosJdbcDAO(connection);
+		//alunosJdbcDAO.salvar(alunos); // este é para inserir na tabela
+		//alunosJdbcDAO.deletar(3); //este é para apagar da tabela, puxa o metodo
+		//alunosJdbcDAO.listar(); //chama o metodo que altera
+		alunosJdbcDAO.listar(); //lista os registros
+	}catch (Exception e) {
+		e.printStackTrace();
+	
+	}
+	
+	
+}
+}
