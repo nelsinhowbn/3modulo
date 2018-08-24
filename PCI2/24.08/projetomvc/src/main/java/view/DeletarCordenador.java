634afb8cc567bda0985a57b709ff65005179d2d6
Aluno <nelsonhadi95@gmail.com>
2018-08-24 20:50:20 -0300

    Janela Deletar

---
+++ PCI2/24.08/projetomvc/src/main/java/view/DeletarCordenador.java
@@ -0,0 +1,64 @@
+package view;
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+import controller.CordenadorJdbcDAO;
+import controller.JdbUtil;
+
+
+public class DeletarCordenador extends JFrame {
+	JButton btnDeletar = new JButton("Deletar");
+	JTextField id = new JTextField();
+	JLabel deletarid = new JLabel("Digite o ID que será Deletado: ");
+	
+	public DeletarCordenador(){
+		super("Apagar Cordenador");
+		
+		Container paine = this.getContentPane();
+		
+		paine.add(deletarid);
+		paine.add(id);	
+		deletarid.setBounds(10, 15, 200, 30);
+		id.setBounds(300, 15, 225, 30);
+		this.setLayout(null);
+		this.setVisible(true);
+		this.setSize(600, 330);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		paine.add(btnDeletar);
+		btnDeletar.setBounds(20, 250, 130, 30);
+		 btnDeletar.setBackground(Color.BLACK); // cOR DO FUNDO DO BOTÃO
+		 btnDeletar.setForeground(Color.WHITE); //fONTE DO BOTÃO SALVAR  
+		
+		btnDeletar.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+			try {
+				Connection connection = JdbUtil.getConnection();
+				CordenadorJdbcDAO deletarcordenadorJdbcDao = new CordenadorJdbcDAO(connection);
+					
+				deletarcordenadorJdbcDao.deletar(Integer.parseInt(id.getText()));
+			}catch(Exception err) { 
+				err.printStackTrace();
+			}
+	
+			}
+		});
+			
+
+	}
+	public static void main(String args[]) {
+		DeletarCordenador dc = new DeletarCordenador();
+	}
+}
+
+
