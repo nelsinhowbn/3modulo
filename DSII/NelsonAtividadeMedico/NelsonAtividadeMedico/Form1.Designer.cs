2e74a52f07e2f666e538e3c8cda022cf6cc70957
nelsinhowbn <nelsonhadi95@gmail.com>
2018-10-01 23:33:35 -0300

    Inserir

---
+++ DSII/NelsonAtividadeMedico/NelsonAtividadeMedico/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace NelsonAtividadeMedico
+{
+    partial class Consulta_Cadastro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.btnConsutar = new System.Windows.Forms.Button();
+            this.dgvPaciente = new System.Windows.Forms.DataGridView();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.txtCPF = new System.Windows.Forms.TextBox();
+            this.txtRG = new System.Windows.Forms.TextBox();
+            this.txtCEP = new System.Windows.Forms.TextBox();
+            this.txtBairro = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtPesquisa = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPaciente)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Codigo Do Paciente:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome Do Paciente:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 338);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "CEP:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "CPF:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 209);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "RG:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 268);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Endereço:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(8, 396);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Bairro";
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(483, 92);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(101, 23);
+            this.btnCadastrar.TabIndex = 7;
+            this.btnCadastrar.Text = "CADASTRAR";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click_1);
+            // 
+            // btnConsutar
+            // 
+            this.btnConsutar.Location = new System.Drawing.Point(483, 136);
+            this.btnConsutar.Name = "btnConsutar";
+            this.btnConsutar.Size = new System.Drawing.Size(101, 23);
+            this.btnConsutar.TabIndex = 8;
+            this.btnConsutar.Text = "CONSULTAR";
+            this.btnConsutar.UseVisualStyleBackColor = true;
+            this.btnConsutar.Click += new System.EventHandler(this.btnConsutar_Click);
+            // 
+            // dgvPaciente
+            // 
+            this.dgvPaciente.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPaciente.Location = new System.Drawing.Point(12, 427);
+            this.dgvPaciente.Name = "dgvPaciente";
+            this.dgvPaciente.Size = new System.Drawing.Size(548, 150);
+            this.dgvPaciente.TabIndex = 9;
+            this.dgvPaciente.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvPaciente_CellDoubleClick);
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(123, 56);
+            this.txtCodigo.Multiline = true;
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(259, 26);
+            this.txtCodigo.TabIndex = 10;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(123, 109);
+            this.txtNome.Multiline = true;
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(259, 23);
+            this.txtNome.TabIndex = 11;
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Location = new System.Drawing.Point(123, 255);
+            this.txtEndereco.Multiline = true;
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(259, 26);
+            this.txtEndereco.TabIndex = 12;
+            // 
+            // txtCPF
+            // 
+            this.txtCPF.Location = new System.Drawing.Point(123, 157);
+            this.txtCPF.Multiline = true;
+            this.txtCPF.Name = "txtCPF";
+            this.txtCPF.Size = new System.Drawing.Size(259, 26);
+            this.txtCPF.TabIndex = 13;
+            // 
+            // txtRG
+            // 
+            this.txtRG.Location = new System.Drawing.Point(123, 195);
+            this.txtRG.Multiline = true;
+            this.txtRG.Name = "txtRG";
+            this.txtRG.Size = new System.Drawing.Size(259, 27);
+            this.txtRG.TabIndex = 14;
+            this.txtRG.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // txtCEP
+            // 
+            this.txtCEP.Location = new System.Drawing.Point(123, 328);
+            this.txtCEP.Multiline = true;
+            this.txtCEP.Name = "txtCEP";
+            this.txtCEP.Size = new System.Drawing.Size(259, 23);
+            this.txtCEP.TabIndex = 15;
+            // 
+            // txtBairro
+            // 
+            this.txtBairro.Location = new System.Drawing.Point(123, 381);
+            this.txtBairro.Multiline = true;
+            this.txtBairro.Name = "txtBairro";
+            this.txtBairro.Size = new System.Drawing.Size(259, 28);
+            this.txtBairro.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(9, 593);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(114, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "PESQUISAR PELO ID";
+            // 
+            // txtPesquisa
+            // 
+            this.txtPesquisa.Location = new System.Drawing.Point(144, 593);
+            this.txtPesquisa.Name = "txtPesquisa";
+            this.txtPesquisa.Size = new System.Drawing.Size(404, 20);
+            this.txtPesquisa.TabIndex = 18;
+            this.txtPesquisa.TextChanged += new System.EventHandler(this.txtPesquisa_TextChanged);
+            // 
+            // Consulta_Cadastro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 615);
+            this.Controls.Add(this.txtPesquisa);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtBairro);
+            this.Controls.Add(this.txtCEP);
+            this.Controls.Add(this.txtRG);
+            this.Controls.Add(this.txtCPF);
+            this.Controls.Add(this.txtEndereco);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.dgvPaciente);
+            this.Controls.Add(this.btnConsutar);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Consulta_Cadastro";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Consulta_Cadastro_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPaciente)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Button btnConsutar;
+        private System.Windows.Forms.DataGridView dgvPaciente;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.TextBox txtCPF;
+        private System.Windows.Forms.TextBox txtRG;
+        private System.Windows.Forms.TextBox txtCEP;
+        private System.Windows.Forms.TextBox txtBairro;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtPesquisa;
+    }
+}
+
---
+++ DSII/NelsonAtividadeMedico/NelsonAtividadeMedico/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace NelsonAtividadeMedico
+{
+    public partial class Consulta_Cadastro : Form
+    {
+        public Consulta_Cadastro()
+        {
+            InitializeComponent();
+        }
+        private void retornarPacientes()
+        {
+            SqlConnection conexao = new SqlConnection();
+            conexao.ConnectionString = "Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=AtividadePaciente;Data Source=LAB-07-11";
+            conexao.Open();
+
+            string comandoSQL = "SELECT * FROM paciente";
+            SqlCommand sqlCommand = new SqlCommand(comandoSQL, conexao);
+            
+            SqlDataAdapter sda = new SqlDataAdapter(sqlCommand);
+            DataTable dt = new DataTable();
+
+            sda.Fill(dt);
+
+            dgvPaciente.DataSource = dt;
+
+            conexao.Close();
+
+
+        }
+        private void CadastrarPaciente(int id_paciente, string nm_paciente,  string cpf_paciente, string rg_paciente, string endereco_paciente,  string cep_paciente, string bairro_paciente)
+        {
+           
+            SqlConnection conexao = new SqlConnection();
+            conexao.ConnectionString = "Password=info211;Persist Security Info=True;User ID=sa;Initial Catalog=AtividadePaciente;Data Source=LAB-07-11";
+
+            conexao.Open();
+
+           
+            string InsertPaciente = string.Concat("insert into paciente values ( '", id_paciente, "','",nm_paciente,"','", cpf_paciente, "', '" ,rg_paciente, "','", endereco_paciente, "','", cep_paciente, "','", bairro_paciente,"' )");
+
+            //insere texto de insert em nova consulta
+            SqlCommand comandaSQL = new SqlCommand(InsertPaciente, conexao);
+
+            //Tecla F5
+            comandaSQL.ExecuteNonQuery();
+
+            //Fechando a Conexão
+            conexao.Close();
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Consulta_Cadastro_Load(object sender, EventArgs e)
+        {
+           
+          
+            retornarPacientes();
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCadastrar_Click_1(object sender, EventArgs e)
+        {
+            int id_paciente = Convert.ToInt32(txtCodigo.Text);
+            CadastrarPaciente(id_paciente, txtNome.Text,txtCPF.Text, txtRG.Text, txtEndereco.Text, txtCEP.Text, txtBairro.Text );
+            //LimparCampos();
+            retornarPacientes();
+            MessageBox.Show("Registro Paciente", "Etec ZL");
+        }
+
+        private void dgvPaciente_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+           // txtCodigo.Text = dgvPaciente.Rows[e.RowIndex].Cells[1].Value.ToString();
+           //txtNome.Text = dgvPaciente.Rows[e.RowIndex].Cells[2].Value.ToString();
+
+
+
+
+        }
+
+        private void txtPesquisa_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnConsutar_Click(object sender, EventArgs e)
+        {
+            retornarPacientes();
+
+        }
+    }
+}
