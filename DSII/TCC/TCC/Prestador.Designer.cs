9642333d0191639f58a6c39b60590104627686cc
Aluno <nelsonhadi95@gmail.com>
2018-10-08 22:42:28 -0300

    Projeto

---
+++ DSII/TCC/TCC/Prestador.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace TCC
+{
+    partial class Prestador
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtCPF = new System.Windows.Forms.TextBox();
+            this.txtEnd = new System.Windows.Forms.TextBox();
+            this.txtCEP = new System.Windows.Forms.TextBox();
+            this.txtCel = new System.Windows.Forms.TextBox();
+            this.txtTipo = new System.Windows.Forms.TextBox();
+            this.grvPrestador = new System.Windows.Forms.DataGridView();
+            this.btnCad = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.grvPrestador)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CPF:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Endereço:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "CEP:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 172);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Celular:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(22, 209);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Tipo de Serviço:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(110, 29);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(363, 20);
+            this.txtNome.TabIndex = 6;
+            // 
+            // txtCPF
+            // 
+            this.txtCPF.Location = new System.Drawing.Point(110, 64);
+            this.txtCPF.Name = "txtCPF";
+            this.txtCPF.Size = new System.Drawing.Size(363, 20);
+            this.txtCPF.TabIndex = 7;
+            // 
+            // txtEnd
+            // 
+            this.txtEnd.Location = new System.Drawing.Point(110, 100);
+            this.txtEnd.Name = "txtEnd";
+            this.txtEnd.Size = new System.Drawing.Size(363, 20);
+            this.txtEnd.TabIndex = 8;
+            // 
+            // txtCEP
+            // 
+            this.txtCEP.Location = new System.Drawing.Point(110, 136);
+            this.txtCEP.Name = "txtCEP";
+            this.txtCEP.Size = new System.Drawing.Size(363, 20);
+            this.txtCEP.TabIndex = 9;
+            // 
+            // txtCel
+            // 
+            this.txtCel.Location = new System.Drawing.Point(110, 172);
+            this.txtCel.Name = "txtCel";
+            this.txtCel.Size = new System.Drawing.Size(363, 20);
+            this.txtCel.TabIndex = 10;
+            // 
+            // txtTipo
+            // 
+            this.txtTipo.Location = new System.Drawing.Point(110, 209);
+            this.txtTipo.Name = "txtTipo";
+            this.txtTipo.Size = new System.Drawing.Size(363, 20);
+            this.txtTipo.TabIndex = 11;
+            // 
+            // grvPrestador
+            // 
+            this.grvPrestador.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grvPrestador.Location = new System.Drawing.Point(28, 250);
+            this.grvPrestador.Name = "grvPrestador";
+            this.grvPrestador.Size = new System.Drawing.Size(445, 150);
+            this.grvPrestador.TabIndex = 12;
+            this.grvPrestador.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.grvPrestador.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // btnCad
+            // 
+            this.btnCad.Location = new System.Drawing.Point(28, 424);
+            this.btnCad.Name = "btnCad";
+            this.btnCad.Size = new System.Drawing.Size(95, 36);
+            this.btnCad.TabIndex = 13;
+            this.btnCad.Text = "CADASTRAR";
+            this.btnCad.UseVisualStyleBackColor = true;
+            this.btnCad.Click += new System.EventHandler(this.btnCad_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(158, 424);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(96, 36);
+            this.btnLimpar.TabIndex = 14;
+            this.btnLimpar.Text = "LIMPAR";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // Prestador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 596);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCad);
+            this.Controls.Add(this.grvPrestador);
+            this.Controls.Add(this.txtTipo);
+            this.Controls.Add(this.txtCel);
+            this.Controls.Add(this.txtCEP);
+            this.Controls.Add(this.txtEnd);
+            this.Controls.Add(this.txtCPF);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Prestador";
+            this.Text = "Prestador";
+            this.Load += new System.EventHandler(this.Prestador_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grvPrestador)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtCPF;
+        private System.Windows.Forms.TextBox txtEnd;
+        private System.Windows.Forms.TextBox txtCEP;
+        private System.Windows.Forms.TextBox txtCel;
+        private System.Windows.Forms.TextBox txtTipo;
+        private System.Windows.Forms.DataGridView grvPrestador;
+        private System.Windows.Forms.Button btnCad;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
---
+++ DSII/TCC/TCC/Prestador.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace TCC
+{
+    public partial class Prestador : Form
+    {
+        public Prestador()
+        {
+            InitializeComponent();
+        }
+        private void retornarPestador()
+        {
+            SqlConnection conexao = new SqlConnection();
+            conexao.ConnectionString = "Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=Tcc;Data Source=LAB-07-11";
+            conexao.Open();
+
+            string comandoSQL = "SELECT * FROM tb_Prestador";
+
+            SqlCommand sqlCommand = new SqlCommand(comandoSQL, conexao);
+
+            SqlDataAdapter sda = new SqlDataAdapter(sqlCommand);
+            DataTable dt = new DataTable();
+
+            sda.Fill(dt);
+
+            grvPrestador.DataSource = dt;
+
+            conexao.Close();
+
+        }
+        private void CadastrarPrestador(string nome, string cpf, string endereco, string cep, string celular, string tipo_servico)
+        {
+            //conexão com banco
+            SqlConnection conexao = new SqlConnection();
+            conexao.ConnectionString = "Password=info211;Persist Security Info=True;User ID=sa;Initial Catalog=Tcc;Data Source=LAB-07-11";
+
+            //abrir conexão
+            conexao.Open();
+
+            //inseri texto de insert
+            string InsertFuncionario = string.Concat("insert into tb_Prestador values ( '", nome, "','", cpf, "','", endereco, "','", cep, "','",celular,"','",tipo_servico,"')");
+
+            //insere texto de insert em nova consulta
+            SqlCommand comandaSQL = new SqlCommand(InsertFuncionario, conexao);
+
+            //Tecla F5
+            comandaSQL.ExecuteNonQuery();
+
+            //Fechando a Conexão
+            conexao.Close();
+        }
+        private void LimparCampos()
+        {
+
+            txtNome.Text = "";
+            txtCEP.Text = "";
+            txtEnd.Text = "";
+             txtCel.Text = "";
+             txtTipo.Text = "";
+             txtCPF.Text = "";
+
+        }
+
+        private void Prestador_Load(object sender, EventArgs e)
+        {
+            retornarPestador();
+        }
+
+        private void btnCad_Click(object sender, EventArgs e)
+        {
+
+           CadastrarPrestador(txtNome.Text, txtCPF.Text, txtEnd.Text, txtCEP.Text, txtCel.Text, txtTipo.Text);
+            LimparCampos();
+            MessageBox.Show("Registro Prestador", "Etec ZL");
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+
+            LimparCampos();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            retornarPestador();
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtNome.Text = grvPrestador.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txtCPF.Text = grvPrestador.Rows[e.RowIndex].Cells[2].Value.ToString();
+            txtEnd.Text = grvPrestador.Rows[e.RowIndex].Cells[3].Value.ToString();
+            txtCEP.Text = grvPrestador.Rows[e.RowIndex].Cells[4].Value.ToString();
+            txtCel.Text = grvPrestador.Rows[e.RowIndex].Cells[5].Value.ToString();
+            txtTipo.Text =  grvPrestador.Rows[e.RowIndex].Cells[6].Value.ToString();
+        }
+    }
+}
